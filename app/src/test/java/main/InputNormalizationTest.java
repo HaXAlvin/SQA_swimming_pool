763d47ebb588696c8409d5a5d75c0f152c5bd760
CHUN-YU, HSUEH <48896687+HaXAlvin@users.noreply.github.com>
2021-06-08 18:27:07 +0800

    private constructor

--- app/src/test/java/main/InputNormalizationTest.java
+++ app/src/test/java/main/InputNormalizationTest.java
@@ -1,23 +1,42 @@
 package main;
 import org.junit.jupiter.api.*;
+
+import java.lang.reflect.*;
+
 public class InputNormalizationTest {
     @DisplayName("時間解析測試")
     @Nested
-    class TimeExtractTest{
+    class TimeExtractTest {
         @Test
-         void extractHourTest(){
+        void extractHourTest() {
             String dateTime = "2021-05-26 週三 14:30:00";
-            Assertions.assertEquals(14,InputNormalization.extractHour(dateTime));
+            Assertions.assertEquals(14, InputNormalization.extractHour(dateTime));
         }
+
         @Test
-         void extractMinTest(){
+        void extractMinTest() {
             String dateTime = "2021-05-26 週三 14:30:00";
-            Assertions.assertEquals(30,InputNormalization.extractMin(dateTime));
+            Assertions.assertEquals(30, InputNormalization.extractMin(dateTime));
         }
+
         @Test
-         void extractWeekTest(){
+        void extractWeekTest() {
             String dateTime = "2021-05-26 週三 14:30:00";
-            Assertions.assertEquals("週三",InputNormalization.extractWeek(dateTime));
+            Assertions.assertEquals("週三", InputNormalization.extractWeek(dateTime));
+        }
+    }
+
+    @DisplayName("私有建構子")
+    @Nested
+    class PrivateConstructor {
+        @Test
+        public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+            Constructor<InputNormalization> constructor = InputNormalization.class.getDeclaredConstructor();
+            Assertions.assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+            constructor.setAccessible(true);
+            Assertions.assertThrows(Exception.class,()->{
+                constructor.newInstance();
+            });
         }
     }
 }
---
+++ app/src/test/java/main/QueryTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.junit.jupiter.api.*;
+import java.lang.reflect.*;
+
+public class QueryTest {
+    @DisplayName("私有建構子")
+    @Nested
+    class PrivateConstructor {
+        @Test
+        public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+            Constructor<Query> constructor = Query.class.getDeclaredConstructor();
+            Assertions.assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+            constructor.setAccessible(true);
+            Assertions.assertThrows(Exception.class,()->{
+                constructor.newInstance();
+            });
+        }
+    }
+}
