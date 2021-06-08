f2aa2922920a0aea2bfbcf5c50c765d884dd0a85
CHUN-YU, HSUEH <48896687+HaXAlvin@users.noreply.github.com>
2021-06-08 17:10:47 +0800

    throwable to exception

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -6,11 +6,9 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("""
-				游泳池收費標準:
-				(1) 一般票價 200 (2) 星期六日250元 (3) 12歲以下, 60歲(含)以上打八折, 限定 3-75 歲可入內游泳
-				(4) 七點以前八折 (營業時間:05:00-22:00) (5) 團體打七折 (6) 會員打五折 (7) 各打折不得重疊使用
-				""");
+		System.out.println("游泳池收費標準:\n"
+				+ "(1) 一般票價 200 (2) 星期六日250元 (3) 12歲以下, 60歲(含)以上打八折, 限定 3-75 歲可入內游泳\n"
+				+ "(4) 七點以前八折 (營業時間:05:00-22:00) (5) 團體打七折 (6) 會員打五折 (7) 各打折不得重疊使用\n");
 		int age = Query.queryAge();
 		boolean memberAuth = Query.queryMember();
 		boolean groupAuth = Query.queryGroup();
